1c58f41b6041af3ac47c35c9c2deca7154f9f6ed
Seb@AlphaSapphire <sebjpierre@gmail.com>
2019-09-27 17:03:15 -0400

    back prop

--- minst_nn/assignment.py
+++ minst_nn/assignment.py
@@ -6,27 +6,27 @@ from preprocess import get_data
 class Model:
     """
     This model class will contain the architecture for
-    your single layer Neural Network for classifying MNIST with 
-    batched learning. Please implement the TODOs for the entire 
-    model but do not change the method and constructor arguments. 
-    Make sure that your Model class works with multiple batch 
-    sizes. Additionally, please exclusively use NumPy and 
+    your single layer Neural Network for classifying MNIST with
+    batched learning. Please implement the TODOs for the entire
+    model but do not change the method and constructor arguments.
+    Make sure that your Model class works with multiple batch
+    sizes. Additionally, please exclusively use NumPy and
     Python built-in functions for your implementation.
     """
 
     def __init__(self):
         # TODO: Initialize all hyperparametrs
-        self.input_size = None # Size of image vectors
-        self.num_classes = None # Number of classes/possible labels
-        self.batch_size = None
-        self.learning_rate = None
+        self.input_size = 784 # Size of image vectors
+        self.num_classes = 10 # Number of classes/possible labels
+        self.batch_size = 100
+        self.learning_rate = .5
 
         # TODO: Initialize weights and biases
-        self.W = None
-        self.b = None
+        self.W = np.zeros((self.num_classes, self.input_size))
+        self.b = np.zeros(self.num_classes)
 
     def call(self, inputs):
-         """
+        """
         Does the forward pass on an batch of input images.
         :param inputs: normalized (0.0 to 1.0) batch of images,
                        (batch_size x 784) (2D), where batch can be any number.
@@ -34,58 +34,86 @@ class Model:
         """
         # TODO: Write the forward pass logic for your model
         # TODO: Calculate, then return, the probability for each class per image using the Softmax equation
+        L = np.ndarray((self.batch_size, self.num_classes), dtype=np.float32)
+        for i in range(self.batch_size):
+            l = np.sum((self.W * inputs[i]), axis=1)
+            l = np.add(l, self.b)
+            L[i] = l.transpose()
 
-        pass
-    
+        L = np.exp(L.transpose())
+        P = np.transpose(L / np.sum(L, axis=0))
+
+        return P
+
     def loss(self, probabilities, labels):
         """
         Calculates the model cross-entropy loss after one forward pass.
-        Loss should be decreasing with every training loop (step). 
-        NOTE: This function is not actually used for gradient descent 
-        in this assignment, but is a sanity check to make sure model 
+        Loss should be decreasing with every training loop (step).
+        NOTE: This function is not actually used for gradient descent
+        in this assignment, but is a sanity check to make sure model
         is learning.
-        :param probabilities: matrix that contains the probabilities 
+        :param probabilities: matrix that contains the probabilities
         per class per image of each class for each image
         :param labels: the true batch labels
         :return: average loss per batch element (float)
         """
         # TODO: calculate average cross entropy loss for a batch
+        total_loss = 0
+        for e in range(self.batch_size):
+            print("label of image " + str(e) + " is " + str(labels[e]))
 
-        pass
-    
+            print("probability guessed " + str(labels[e]) + ": " +
+            str(probabilities[e][labels[e]]))
+
+            error = -np.log(probabilities[e][labels[e]])
+            print("loss for example " + str(e) + ": " + str(error) + "\n")
+
+            total_loss += error
+
+        print("TOTAL loss this batch: " + str(total_loss))
+
+        avg_loss = total_loss / self.batch_size
+        print("AVERAGE LOSS THIS BATCH: " + str(avg_loss))
+
+        return avg_loss
+
     def back_propagation(self, inputs, probabilities, labels):
         """
-        Returns the gradients for model's weights and biases 
-        after one forward pass and loss calculation. The learning 
-        algorithm for updating weights and biases mentioned in 
-        class works for one image, but because we are looking at 
+        Returns the gradients for model's weights and biases
+        after one forward pass and loss calculation. The learning
+        algorithm for updating weights and biases mentioned in
+        class works for one image, but because we are looking at
         batch_size number of images at each step, you should take the
         average of the gradients across all images in the batch.
         :param inputs: batch inputs (a batch of images)
-        :param probabilities: matrix that contains the probabilities of each 
+        :param probabilities: matrix that contains the probabilities of each
         class for each image
         :param labels: true labels
         :return: gradient for weights,and gradient for biases
         """
         # TODO: calculate the gradients for the weights and the gradients for the bias with respect to average loss
-    
+        #print(probabilities.shape)
+        one_hot = np.zeros((self.batch_size, self.num_classes))
+        print(one_hot.shape)
+
+
         pass
-    
+
     def accuracy(self, probabilities, labels):
         """
-        Calculates the model's accuracy by comparing the number 
+        Calculates the model's accuracy by comparing the number
         of correct predictions with the correct answers.
         :param probabilities: result of running model.call() on test inputs
         :param labels: test set labels
         :return: Float (0,1) that contains batch accuracy
         """
         # TODO: calculate the batch accuracy
-        
+
         pass
 
     def gradient_descent(self, gradW, gradB):
         '''
-        Given the gradients for weights and biases, does gradient 
+        Given the gradients for weights and biases, does gradient
         descent on the Model's parameters.
         :param gradW: gradient for weights
         :param gradB: gradient for biases
@@ -94,35 +122,35 @@ class Model:
         # TODO: change the weights and biases of the model to descent the gradient
 
         pass
-    
+
 def train(model, train_inputs, train_labels):
     '''
     Trains the model on all of the inputs and labels.
-    :param model: the initialized model to use for the forward 
+    :param model: the initialized model to use for the forward
     pass and backward pass
     :param train_inputs: train inputs (all inputs to use for training)
     :param train_inputs: train labels (all labels to use for training)
     :return: None
     '''
-    
+
     # TODO: Iterate over the training inputs and labels, in model.batch_size increments
     # TODO: For every batch, compute then descend the gradients for the model's weights
-    
+
     pass
 
 def test(model, test_inputs, test_labels):
     """
-    Tests the model on the test inputs and labels. For this assignment, 
+    Tests the model on the test inputs and labels. For this assignment,
     the inputs should be the entire test set, but in the future we will
     ask you to batch it instead.
     :param test_inputs: MNIST test data (all images to be tested)
     :param test_labels: MNIST test labels (all corresponding labels)
     :return: accuracy - Float (0,1)
     """
-    
+
     # TODO: Iterate over the testing inputs and labels
     # TODO: Return accuracy across testing set
-    
+
     pass
 
 def visualize_results(image_inputs, probabilities, image_labels):
@@ -134,7 +162,7 @@ def visualize_results(image_inputs, probabilities, image_labels):
 
     NOTE: DO NOT EDIT
 
-    :return: doesn't return anything, a plot should pop-up 
+    :return: doesn't return anything, a plot should pop-up
     """
     images = np.reshape(image_inputs, (-1, 28, 28))
     predicted_labels = np.argmax(probabilities, axis=1)
@@ -152,11 +180,16 @@ def visualize_results(image_inputs, probabilities, image_labels):
 
 def main():
     '''
-    Read in MNIST data, initialize your model, and train and test your model 
-    for one epoch. The number of training steps should be your the number of 
+    Read in MNIST data, initialize your model, and train and test your model
+    for one epoch. The number of training steps should be your the number of
     batches you run through in a single epoch. You should receive a final accuracy on the testing examples of > 80%.
     :return: None
     '''
+    inputs, labels = get_data('MNIST_data/t10k-images-idx3-ubyte.gz', 'MNIST_data/t10k-labels-idx1-ubyte.gz', 10000)
+    model = Model()
+    probs = model.call(inputs)
+    model.loss(probs, labels)
+    model.back_propagation(inputs, probs, labels)
 
     # TODO: load MNIST train and test examples into train_inputs, train_labels, test_inputs, test_labels
 
@@ -169,6 +202,6 @@ def main():
     # TODO: Visualize the data by using visualize_results()
 
     pass
-    
+
 if __name__ == '__main__':
     main()
--- minst_nn/preprocess.py
+++ minst_nn/preprocess.py
@@ -21,5 +21,16 @@ def get_data(inputs_file_path, labels_file_path, num_examples):
 	:return: NumPy array of inputs as float32 and labels as int8
 	"""
 
+
 	#TODO: Load inputs and labels
 	#TODO: Normalize inputs
+	with open(inputs_file_path, 'rb') as f, gzip.GzipFile(fileobj=f) as img_file:
+		img_file.read(16) #reads the 1st 16 bytes that constitute the file header
+		images = (1/255.0 * np.frombuffer(img_file.read(), dtype=np.uint8))
+		images = images.reshape((num_examples, 784))
+
+	with open(labels_file_path, 'rb') as g, gzip.GzipFile(fileobj=g) as labls_file:
+		labls_file.read(8); #reads the 1st 8 bytes that constitute the file header
+		labels = np.frombuffer(labls_file.read(), dtype=np.uint8)
+
+	return (images, labels)
