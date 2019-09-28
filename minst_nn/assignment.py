c533e808c98247c8102b9e9711c626e47cc10194
Seb@AlphaSapphire <sebjpierre@gmail.com>
2019-09-28 09:51:40 -0400

    forgot accuracy calc

--- minst_nn/assignment.py
+++ minst_nn/assignment.py
@@ -224,8 +224,8 @@ def main():
     test_inputs, test_labels = get_data('MNIST_data/t10k-images-idx3-ubyte.gz', 'MNIST_data/t10k-labels-idx1-ubyte.gz', 10000)
     model = Model()
     train(model, train_inputs, train_labels)
-    #accuracy = test(model, test_inputs, test_labels)
-    #print("accuracy: " + str((accuracy) * 100) + "%")
+    accuracy = test(model, test_inputs, test_labels)
+    print("accuracy: " + str((accuracy) * 100) + "%")
 
 
     # TODO: load MNIST train and test examples into train_inputs, train_labels, test_inputs, test_labels
